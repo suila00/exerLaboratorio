30ddf261f4d66d3707c47a9a0f0ffb82ef62dcec
suila <suilasilva00@gmail.com>
2022-03-20 22:51:49 +0000

    class main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Porto porto = new Porto(3);
+        Porto porto = new Porto(5);
         NavioPetroleiro petro1 = new NavioPetroleiro("1234"); petro1.setCarga(200);
-        PortaContentores pConta1 = new PortaContentores("2345"); pConta1.setNoContentores(50);
-        PortaContentores pConta2 = new PortaContentores("3456"); pConta2.setNoContentores(20);
+        PortaContentores pConta1 = new PortaContentores("5678"); pConta1.setNoContentores(50);
+        PortaContentores pConta2 = new PortaContentores("9012"); pConta2.setNoContentores(20);
         Navio n = new Navio("0123");
         NavioPetroleiro petro2 = new NavioPetroleiro("1234"); petro2.setCarga(200);
         porto.novoNavio(petro1);
