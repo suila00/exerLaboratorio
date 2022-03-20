e1631a1734777a7f7f62b5811cf7e1c757775574
suila <suilasilva00@gmail.com>
2022-03-20 22:47:10 +0000

    class main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Porto porto = new Porto(3);
+        NavioPetroleiro petro1 = new NavioPetroleiro("1234"); petro1.setCarga(200);
+        PortaContentores pConta1 = new PortaContentores("2345"); pConta1.setNoContentores(50);
+        PortaContentores pConta2 = new PortaContentores("3456"); pConta2.setNoContentores(20);
+        Navio n = new Navio("0123");
+        NavioPetroleiro petro2 = new NavioPetroleiro("1234"); petro2.setCarga(200);
+        porto.novoNavio(petro1);
+        porto.novoNavio(petro2);
+        porto.novoNavio(pConta1);
+        porto.novoNavio(pConta2);
+        porto.novoNavio(n);
+        System.out.println("Capacidade total = " + porto.getCapacidadeTotal() + " toneladas.");
     }
 }
